9a13e122d1d4475782fb2349193fb5f597a45163
Cristian Molina <megatux@gmail.com>
2024-03-15 09:38:28 -0300

    Fix tests

--- test/controllers/articles_controller_test.rb
+++ test/controllers/articles_controller_test.rb
@@ -2,12 +2,15 @@ require "test_helper"
 
 class ArticlesControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get articles_index_url
+    get articles_url
     assert_response :success
+    assert_match "id=\"article-list\"", @response.body
   end
 
-  test "should get show" do
-    get articles_show_url
+  test "adding an article" do
+    post articles_url, xhr: true
     assert_response :success
+    assert_match "text/html", @response.media_type
+    assert_match "item-article", @response.body
   end
 end
