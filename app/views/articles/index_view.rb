de7403ca958ddad6b2fcf2f4cc8d4adeca6963b8
Cristian Molina <megatux@gmail.com>
2024-03-15 00:16:38 -0300

    Some small refactors

--- app/views/articles/index_view.rb
+++ app/views/articles/index_view.rb
@@ -1,20 +1,29 @@
 # frozen_string_literal: true
 
 class Articles::IndexView < ApplicationView
+  HEAD_STYLE = "p-2 font-mono font-bold font-lg font-bold"
+  BUTTON_STYLE = "font-mono rounded border-2 border-black bg-indigo-200 p-1 text-sm transition-colors focus-ring-2 focus:ring-indigo-800 hover:bg-indigo-100"
+
   def template
     div(class: "h-16 flex items-center justify-between") {
-      h1(class: "p-2 font-mono font-bold font-lg font-bold") { "Articles index" }
+      h1(class: HEAD_STYLE) { "Articles index" }
 
       button("hx-post": articles_path, "hx-target": "#article-list", "hx-swap": "afterbegin",
-        class: "font-mono rounded border-2 border-black bg-indigo-200 p-1 text-sm transition-colors focus-ring-2 focus:ring-indigo-800 hover:bg-indigo-100") {
+        class: BUTTON_STYLE) {
         plain "Add new article"
       }
     }
 
     section(id: "article-list") {
-      3.times do
-        render ArticleComponent.new
-      end
+      articles.each { |el| render el }
     }
+  end
+
+  private
+
+  def articles
+    3.times.map do
+      ArticleComponent.new
+    end
   end
 end
--- app/views/components/article_component.rb
+++ app/views/components/article_component.rb
@@ -3,9 +3,12 @@
 require "faker"
 
 class ArticleComponent < ApplicationComponent
+  ARTICLE_STYLE = "mb-2 bg-blue-600 rounded border-solid border-2 border-black item-article"
+  HEADER_STYLE = "text-left text-slate-200 pl-2 font-semibold"
+
   def template
-    article(id: Time.now.to_i, class: "mb-2 bg-blue-600 rounded border-solid border-2 border-black item-article") {
-      h3(class: "text-left text-slate-200 pl-2 font-semibold") {
+    article(class: ARTICLE_STYLE) {
+      h3(class: HEADER_STYLE) {
         i { Faker::Lorem.sentence }
       }
       p(class: "bg-slate-200 p-4") { Faker::Lorem.paragraph }
